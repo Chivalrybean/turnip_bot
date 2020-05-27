e3f43b1ca673e80c0fa2250a92036adc3a194204
Lowmaster <Justin.Lowmaster@flir.com>
2020-05-27 14:47:29 -0700

    Added basic function info text.

--- turnip_bot.py
+++ turnip_bot.py
@@ -20,11 +20,13 @@ class island:
 
 
 def save_data():
+    """Saves turnip_data to .json file"""
     with open('turnip_file.json', 'w') as f:
         json.dump(turnip_data, f)
 
 
 def load_data():
+    """Loads .json file data to turnip_data, unless no file present, then it creates a new file and puts current data into it."""
     try:
         with open('turnip_file.json', 'r') as f:
             turnip_data = json.load(f)
@@ -35,6 +37,8 @@ def load_data():
 
 
 def generate_list(server, channel):
+    """Creates a response to return to a Discord server and channel of the listed island to visit, if any.
+    Will also generate list to edit when updated when island invites expire."""
     response = "-" * 40 + "\n"
     try:
         this_list = turnip_data[server][channel]
@@ -47,6 +51,7 @@ def generate_list(server, channel):
 
 
 def add_island(server, channel, island):
+    """Adds an island to the data under the server and channel name"""
     global turnip_data
     if channel in turnip_data.keys():
         if server in turnip_data[server].keys():
