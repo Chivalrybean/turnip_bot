120e18f386bd7b4800551cb8ad7a62eec7f49167
Chivalrybean <jlowmaster@gmail.com>
2020-06-06 00:23:05 -0700

    Back to UserName, can't pickle TaskStepMethWrapper Back to storing user.name in an Island

--- turnip_bot.py
+++ turnip_bot.py
@@ -174,7 +174,7 @@ async def on_message(message):
                 temp_msgs.append(msg)
             return msg.channel == channel and msg.author == user
 
-        island_info = [user]
+        island_info = [user.name]
         for question in island_questions:
             try:
                 msg = await client.wait_for('message', timeout=60.0, check=check)
